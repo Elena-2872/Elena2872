8e2225333603583099d62543efc8442a54c5992f
Елена Бессараб <eva-72@mail.ru>
2024-08-27 23:31:36 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,8 @@
+
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])   # добавляем список [2]
+    for j in range(m):
+        matrix[i].append(value) # пишем в список значение [2]
+    print(matrix)
