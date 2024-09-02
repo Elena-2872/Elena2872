7cd502f5df5368c3340ef41a181cc1d5dfd63d18
Елена Бессараб <eva-72@mail.ru>
2024-09-02 14:03:40 +0300

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -3,9 +3,9 @@ def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
         matrix.append([])   # добавляем список
-    for j in range(m):
-        matrix[i].append(value) # пишем в список значение
-    print([matrix[i], matrix[j]])
-get_matrix(2, 2, 10)
+        for j in range(m):
+            matrix[i].append(value) # пишем в список значение
+    return [matrix[i], matrix[j]]
+print(get_matrix(2, 2, 10))
 
 
