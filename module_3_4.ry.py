1e40217c48d83a6870fb9312a3ca9511e0a87404
Елена Бессараб <eva-72@mail.ru>
2024-09-08 13:10:37 +0300

    Add files via upload

---
+++ module_3_4.ry.py
@@ -0,0 +1,15 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for word in other_words:
+        if root_word.startswith(word) or word.startswith(root_word):
+            same_words.append(word)
+    return same_words
+print(single_root_words('rich', 'richest', 'orichalcum', 'cheers', 'riches'))
+# ['richest', 'orichalcum', 'riches']
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
+# ['Disable', 'Able']
+
+
+
+
+
