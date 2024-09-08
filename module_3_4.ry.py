b7aa88e2db0b9b12bfd89921569e9fc93c321a23
Елена Бессараб <eva-72@mail.ru>
2024-09-08 13:17:07 +0300

    Add files via upload

--- module_3_4.ry.py
+++ module_3_4.ry.py
@@ -1,7 +1,7 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for word in other_words:
-        if root_word.startswith(word) or word.startswith(root_word):
+        if root_word.lower().count(word.lower()) or word.lower().count(root_word.lower()):
             same_words.append(word)
     return same_words
 print(single_root_words('rich', 'richest', 'orichalcum', 'cheers', 'riches'))
